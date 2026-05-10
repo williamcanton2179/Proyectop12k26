212df3fb487feebab812a18bda72116f7d151835
Moises De Leon <mdeleonp25@miumg.edu.gt>
2026-05-10 12:16:54 -0600

    modificacion bancos 10/05

--- proyectop12k26/src/Bancos.cpp
+++ proyectop12k26/src/Bancos.cpp
@@ -12,6 +12,53 @@ Bancos::Bancos()
     //ctor
 }
 
+bool Bancos::InfoPagoPlanilla(string nombreBancoReq, string nombreClienteReq, int idCuentaReq)
+{
+    fstream file;
+    int idArch;
+    string clienteArch, bancoArch;
+    double saldoArch, movArch;
+    long long tarjetaArch;
+    bool encontrado = false;
+
+    file.open("CuentasBancarias.txt", ios::in);
+
+    if (file.is_open())
+    {
+        while (file >> idArch >> clienteArch >> saldoArch >> movArch >> tarjetaArch >> bancoArch)
+        {
+            if (idArch == idCuentaReq)
+            {
+                this->idCuenta = idArch;
+                this->nombreCliente = clienteArch;
+                this->saldo = saldoArch;
+                this->nombreBanco = bancoArch;
+                this->numeroTarjeta = tarjetaArch;
+
+                encontrado = true;
+                break;
+            }
+        }
+        file.close();
+    }
+
+    if (encontrado)
+    {
+        cout << "Cuenta encontrada: " << this->nombreCliente << endl;
+        return true;
+    }
+    else
+    {
+        cout << "Error: No se encontro el ID de cuenta." << endl;
+        return false;
+    }
+}
+
+bool Bancos::InfoTransferencia(string nombreBanco, string nombreCliente, long long numeroTarjeta)
+{
+    this->numeroTarjeta = numeroTarjeta
+}
+
 bool Bancos::crearCuenta(string nombreCliente, double monto, string nombreBanco)
 {
     this->idCuenta = generadorTarjetasCuentas();
